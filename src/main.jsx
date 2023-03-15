4b0c1498f387f528364e1acc1a903f188f68164e
GUSTAV0DEDEUS <88512041+GUSTAV0DEDEUS@users.noreply.github.com>
2023-03-15 12:05:31 -0300

    :tada: First commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import HomePage from './Pages/Homepage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomePage/>
+  </React.StrictMode>,
+)
